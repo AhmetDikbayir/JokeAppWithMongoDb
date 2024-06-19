a4b30c21ccda9f047dfc8759df9d31d9e39151a5
Your Name <ahmet.dikbayir@gmail.com>
2024-06-19 23:47:43 +0300

    joke request updated

--- src/main/java/com/tpe/joke/payload/request/JokeRequest.java
+++ src/main/java/com/tpe/joke/payload/request/JokeRequest.java
@@ -1,5 +1,14 @@
 package com.tpe.joke.payload.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class JokeRequest {
 
     private String kind;
--- src/main/java/com/tpe/joke/payload/response/JokeResponse.java
+++ src/main/java/com/tpe/joke/payload/response/JokeResponse.java
@@ -1,4 +1,17 @@
 package com.tpe.joke.payload.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class JokeResponse {
+
+    private String kind;
+    private String title;
+    private String content;
 }
