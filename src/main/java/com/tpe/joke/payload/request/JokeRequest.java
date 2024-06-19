5ba3dee47323d7657bde298b5dd012794ba41288
Your Name <ahmet.dikbayir@gmail.com>
2024-06-19 23:51:55 +0300

    jokeRequest class updated

--- src/main/java/com/tpe/joke/payload/request/JokeRequest.java
+++ src/main/java/com/tpe/joke/payload/request/JokeRequest.java
@@ -5,13 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class JokeRequest {
 
+    @NotNull(message = "The kind cannot be null.")
     private String kind;
+
+    @NotNull(message = "The title cannot be null.")
     private String title;
+
+    @NotNull(message = "The content cannot be null.")
     private String content;
 }
